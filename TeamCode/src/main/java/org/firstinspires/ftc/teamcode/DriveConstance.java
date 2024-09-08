d51a4f1e3a992353ee78ef67a5089111a876fd3d
KingTryHard1 <codeshb7@gmail.com>
2024-09-07 20:56:33 -0400

    I finished coding

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstance.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public abstract class DriveConstance extends LinearOpMode {
+
+    public DcMotorEx frontLeft;
+    public DcMotorEx frontRight;
+    public DcMotorEx backLeft;
+    public DcMotorEx backRight;
+
+    public void initRobot() {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+    }
+}
