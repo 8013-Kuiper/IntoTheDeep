23858e56257ab615cdc1f194d084551647c616c2
KingTryHard1 <codeshb7@gmail.com>
2024-09-17 09:55:36 -0700

    Added ToggleBool and Test. Deleted Kotlin ToggleBool

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public class test {
+    ToggleBool Switch = new ToggleBool();
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/toggleBool.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class toggleBool{
+    public toggleBool(boolean setInitialVal, int setInputTimeOut){
+        setInitialVal(setInitialVal);
+        setInputTimeOut(setInputTimeOut);
+    }
+    public toggleBool(boolean setInitialVal){
+        setInitialVal(setInitialVal);
+    }
+
+    private boolean bool = false;
+    private double wait = 1000;
+    ElapsedTime timeOut = new ElapsedTime();
+
+    public boolean flipAndReturnBool(boolean gamepad){
+        flipBool(gamepad);
+        return bool;
+    }
+
+    public boolean flipAndReturnBool(boolean gamepad1, boolean gamepad2){
+        flipBool(gamepad1, gamepad2);
+        return bool;
+    }
+
+    public void flipBool(boolean gamepad){
+        if (timeOut.milliseconds()>wait)
+            if (gamepad) {
+                bool = !bool;
+                timeOut.reset();
+            }
+    }
+
+    public void flipBool(){
+        bool = !bool;
+    }
+
+    public void flipBool(boolean gamepad1, boolean gamepad2){
+        if (timeOut.milliseconds()>wait)
+            if (gamepad1 && gamepad2) {
+                bool = !bool;
+                timeOut.reset();
+            }
+    }
+
+    public boolean returnBool(){
+        return bool;
+    }
+
+    public void setInitialVal(boolean initial){
+        bool = initial;
+    }
+
+    public void setInputTimeOut(double  Timeout){
+        Timeout = wait;
+    }
+}
