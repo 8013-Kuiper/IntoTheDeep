48eaa36a306ead492b0465451587fd94a53912d5
KingTryHard1 <codeshb7@gmail.com>
2024-09-18 10:52:37 -0700

    Added a motor test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class motorTest extends DriveConstance{
+    int motorPortNum = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        ElapsedTime wait = new ElapsedTime();
+        initRobot();
+
+
+        waitForStart();
+        if (opModeIsActive()){
+            if (wait.milliseconds()>300) {
+                if (gamepad1.a){
+                    motorPortNum++;
+                    wait.reset();
+                }
+                if (gamepad1.b){
+                    motorPortNum--;
+                    wait.reset();
+                }
+            }
+
+            switch (motorPortNum){
+                case 0:
+                    frontLeft.setPower(gamepad1.right_stick_y);
+                case 1:
+                    frontRight.setPower(gamepad1.right_stick_y);
+                case 2:
+                    backLeft.setPower(gamepad1.right_stick_y);
+                case 3:
+                    backRight.setPower(gamepad1.right_stick_y);
+            }
+            telemetry.addData("motorPortNum: ", motorPortNum);
+            telemetry.update();
+
+        }
+    }
+}
