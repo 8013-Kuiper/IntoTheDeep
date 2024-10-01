7733515fe10de605759eeee3ebc87cc6d0594e56
KingTryHard1 <codeshb7@gmail.com>
2024-10-01 18:34:52 -0400

    More MeepMeep

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
@@ -8,7 +8,7 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(750);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
@@ -18,8 +18,22 @@ public class MeepMeepTesting {
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(38, 62, Math.toRadians(0)))
                         .setTangent(Math.toRadians(270))
                         .splineToConstantHeading(new Vector2d(25,0), Math.toRadians(225))
+                //Drop specimen on bar
                         .setTangent(Math.toRadians(0))
                         .splineToSplineHeading(new Pose2d(47, 20, Math.toRadians(80)), Math.toRadians(90))
+                        //Pick up 1 sample
+                        .splineToSplineHeading(new Pose2d(52,50, Math.toRadians(230)), Math.toRadians(50))
+                //Deposit sample into basket
+                        .setTangent(Math.toRadians(0))
+                        .splineToSplineHeading(new Pose2d(60, 48, Math.toRadians(269)), Math.toRadians(0))
+                //Pick up sample then deposit
+                        .setTangent(Math.toRadians(230))
+                        .splineToSplineHeading(new Pose2d(50, 35, Math.toRadians(340)), Math.toRadians(230))
+                //Reach out and pick up sample
+                        .setTangent(Math.toRadians(110))
+                .splineToSplineHeading(new Pose2d(60, 48, Math.toRadians(269)), Math.toRadians(0))
+                //Deposit sample into basket
+                        //Park
 
                 .build());
 
