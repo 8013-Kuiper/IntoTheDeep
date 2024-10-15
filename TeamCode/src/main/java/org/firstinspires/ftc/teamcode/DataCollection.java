d5a5957a65d9cf324dbb48e33abc4e6942ee0771
KingTryHard1 <codeshb7@gmail.com>
2024-10-15 08:13:31 -0700

    Much code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DataCollection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DataCollection.java
@@ -3,32 +3,53 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import java.util.Dictionary;
-import java.util.Enumeration;
 import java.util.Hashtable;
 
-@TeleOp (group = "Testing")
+@TeleOp (name = "DataCollection", group = "Testing")
 public class DataCollection extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Dictionary<String, Integer> Data = new Hashtable<>();
+        Dictionary<String, Object> Data = new Hashtable<>();
         RevColorSensorV3 intakeColorSensor = hardwareMap.get(RevColorSensorV3.class, "intakeColor");
 
+        float gain = 2F;
+
+        ElapsedTime waitTime = new ElapsedTime();
+
         waitForStart();
-        if (opModeIsActive()) {
-            if (gamepad1.a)
-                Data.put("Blue", intakeColorSensor.argb());
-            if (gamepad1.b)
-                Data.put("Red", intakeColorSensor.argb());
-            if (gamepad1.x)
-                Data.put("Neutral", intakeColorSensor.argb());
+        while (opModeIsActive()) {
+            intakeColorSensor.setGain(gain);
+
+            Data.put("Blue", intakeColorSensor.blue());
+            Data.put("Red", intakeColorSensor.red());
+            Data.put("Green", intakeColorSensor.green());
 
 
+            if (gamepad1.dpad_up) {
+                if (waitTime.milliseconds()>500) {
+                    gain += .05F;
+                    waitTime.reset();
+                }
+            }
+
+            if (gamepad1.dpad_down) {
+                if (waitTime.milliseconds()>500) {
+                    gain -= .05f;
+                    waitTime.reset();
+                }
+            }
+
+            Data.put("Gain", gain);
+
+            telemetry.addData("timer: ", waitTime.milliseconds());
+            telemetry.addData("Gain: ", Data.get("Gain"));
             telemetry.addData("Blue: ", Data.get("Blue"));
             telemetry.addData("Red: ", Data.get("Red"));
-            telemetry.addData("Red: ", Data.get("Neutral"));
+            telemetry.addData("Green: ", Data.get("Green"));
             telemetry.update();
         }
 
