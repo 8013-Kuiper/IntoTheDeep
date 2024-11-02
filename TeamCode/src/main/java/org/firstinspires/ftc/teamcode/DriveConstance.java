1d1d84a61aff0280857cd59a0054047bf3f4a0de
KingTryHard1 <codeshb7@gmail.com>
2024-11-01 17:52:33 -0700

    Axon thing didn't work. Added slide to Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveConstance.java
@@ -12,6 +12,9 @@ public abstract class DriveConstance extends LinearOpMode {
     public DcMotorEx backLeft;
     public DcMotorEx backRight;
 
+    public DcMotorEx leftVertLinear;
+    public DcMotorEx rightVertLinear;
+
     public RevColorSensorV3 intakeColorSensor;
 
     public CRServoImplEx intake;
@@ -21,6 +24,9 @@ public abstract class DriveConstance extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
         backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
         backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        leftVertLinear = hardwareMap.get(DcMotorEx.class, "leftVertLinear");
+        rightVertLinear = hardwareMap.get(DcMotorEx.class, "rightVertLinear");
 
         intakeColorSensor = hardwareMap.get(RevColorSensorV3.class, "intakeColor");
 
