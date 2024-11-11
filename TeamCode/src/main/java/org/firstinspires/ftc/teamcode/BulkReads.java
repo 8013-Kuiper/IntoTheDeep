e894f23f5a4c22281d03667feef60aa4595800a5
KingTryHard1 <codeshb7@gmail.com>
2024-11-11 16:43:25 -0500

    Trying to get bulk reads of just the motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BulkReads.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.util.List;
+
+public class BulkReads extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            for (LynxModule module : allHubs) {
+                telemetry.addData("Thing", module);
+            }
+        }
+    }
+}
