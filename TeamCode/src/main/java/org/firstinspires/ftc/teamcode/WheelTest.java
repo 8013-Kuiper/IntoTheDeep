e7310cc991a4f2c0d213b0b4e5abb370afbd218b
KingTryHard1 <codeshb7@gmail.com>
2024-11-27 14:34:40 -0800

    added break

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelTest.java
@@ -5,6 +5,7 @@ import android.text.method.MultiTapKeyListener;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import java.math.*;
 
 @TeleOp
 public class WheelTest extends DriveConstance{
@@ -12,6 +13,7 @@ public class WheelTest extends DriveConstance{
     @Override
     public void runOpMode() throws InterruptedException {
         ElapsedTime wait = new ElapsedTime();
+        double horizontalPower = gamepad1.left_stick_y;
         initRobot();
 
         enum IntakeLiftE {
@@ -24,9 +26,9 @@ public class WheelTest extends DriveConstance{
         IntakeLiftE intake = IntakeLiftE.start;
         waitForStart();
         while (opModeIsActive()){
-            if (gamepad1.a)
+            if (gamepad2.right_trigger>.1)
                 Wheel.setPower(1);
-            else if (gamepad1.b)
+            else if (gamepad2.left_trigger>.1)
                 Wheel.setPower(-1);
             else
                 Wheel.setPower(0);
@@ -35,7 +37,7 @@ public class WheelTest extends DriveConstance{
             switch (intake){
 
                 case Low:
-                    if(gamepad1.dpad_right){
+                    if(gamepad1.a){
                         //intakeLift.Servo().setPwmEnable();
                         intake = IntakeLiftE.start;
                         break;
@@ -47,14 +49,14 @@ public class WheelTest extends DriveConstance{
                         //intakeLift.Servo().setPwmDisable();
                         intake = IntakeLiftE.Low;
                     }*/
-                    if(gamepad1.dpad_left){
+                    if(gamepad1.b){
                         intake= IntakeLiftE.Low;
                         break;
                     }
 
                 case start:
                    // intakeLift.Servo().setPosition(1);
-                    if (gamepad1.right_trigger>.1){
+                    if (gamepad1.y){
                         wait.reset();
                         intake = IntakeLiftE.Middle;
                         break;
@@ -62,9 +64,9 @@ public class WheelTest extends DriveConstance{
             }
 
 
-            if (gamepad1.x)
+            if (gamepad1.left_bumper)
                 outtakeGrab.setPosition(.5);
-            if (gamepad1.y)
+            if (gamepad1.right_bumper)
                 outtakeGrab.setPosition(0);
 
             if (gamepad1.dpad_up) {
@@ -74,9 +76,32 @@ public class WheelTest extends DriveConstance{
             if (gamepad1.dpad_down) {
                 outtakeSpin.setPosition(0);
                 outtakeFlip.setPosition(0);
+                outtakeGrab.setPosition(0);
             }
 
-            HorizontalLinear.setPower(gamepad1.left_stick_y);
+
+            if(HorizontalLinear.getCurrentPosition()>990) {
+                if (gamepad1.left_stick_y>0){
+                    horizontalPower=0;
+                }
+            }
+
+            HorizontalLinear.setPower(horizontalPower);
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeft.setPower(frontLeftPower);
+            backLeft.setPower(backLeftPower);
+            frontRight.setPower(frontRightPower);
+            backRight.setPower(backRightPower);
 
             telemetry.addData("servopos", intakeLift.Servo().getPosition());
             telemetry.addData("time", wait);
