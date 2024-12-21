27f42445d96c320a03ecc097de994c864c443062
KingTryHard1 <codeshb7@gmail.com>
2024-12-20 16:24:29 -0800

    fuck henrys switch statments

--- MeepMeep/src/main/java/com/example/meepmeep/meepMeepTesting.java
+++ MeepMeep/src/main/java/com/example/meepmeep/meepMeepTesting.java
@@ -42,41 +42,44 @@ public class meepMeepTesting {
 
 
         hookPlusThreeYellow.runAction(hookPlusThreeYellow.getDrive().actionBuilder(blueLeft)
-                        .setTangent(Math.toRadians(270))
-                        .splineToConstantHeading(new Vector2d(8,32), Math.toRadians(225))
-                        .waitSeconds(2)
+                .setTangent(Math.toRadians(270))
+                .splineToConstantHeading(new Vector2d(8,32), Math.toRadians(225))
+                .waitSeconds(2)
                 //Drop specimen on bar
-                        .splineToConstantHeading(new Vector2d(8,40), Math.toRadians(0))
+                .splineToConstantHeading(new Vector2d(8,40), Math.toRadians(0))
                 //back up from bar
-                        .setTangent(Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(30, 25, Math.toRadians(0)), Math.toRadians(225))
-                        //.splineToConstantHeading(new Vector2d(30,25), Math.toRadians(0))
-                        .waitSeconds(2)
-                        //Pick up 1 sample
-                        .setReversed(true)
-                        .splineToSplineHeading(new Pose2d(62,60, Math.toRadians(230)), Math.toRadians(50))
-                        .waitSeconds(2)
+                .setTangent(Math.toRadians(0))
+                .splineToSplineHeading(new Pose2d(25, 15, Math.toRadians(0)), Math.toRadians(179))
+                //.splineToConstantHeading(new Vector2d(30,25), Math.toRadians(0))
+                .waitSeconds(2)
+                //Pick up 1 sample
+                .setReversed(true)
+                .strafeToConstantHeading(new Vector2d(25,25))
+                .splineToSplineHeading(new Pose2d(57,55, Math.toRadians(230)), Math.toRadians(50))
+                .waitSeconds(2)
                 //Deposit sample into basket
-                        .setTangent(Math.toRadians(0))
-                        .setReversed(false)
-                        .splineToLinearHeading(new Pose2d(40, 25, Math.toRadians(0)), Math.toRadians(0))
-                        .waitSeconds(2)
+                .setTangent(Math.toRadians(0))
+                .setReversed(false)
+                .splineToLinearHeading(new Pose2d(35, 15, Math.toRadians(0)), Math.toRadians(0))
+                .waitSeconds(2)
                 //move to pick up another sample (2)
-                        .setReversed(true)
-                        .splineToSplineHeading(new Pose2d(62,60, Math.toRadians(230)), Math.toRadians(50))
-                        .waitSeconds(2)
+                .setReversed(true)
+                        .setTangent(Math.toRadians(110))
+                .splineToSplineHeading(new Pose2d(57,55, Math.toRadians(230)), Math.toRadians(50))
+                .waitSeconds(2)
                 //Deposit sample into basket
-                        .setReversed(false)
-                        .splineToLinearHeading(new Pose2d(50, 25, Math.toRadians(0)), Math.toRadians(0))
-                        .waitSeconds(2)
+                .setTangent(Math.toRadians(0))
+                .setReversed(false)
+                .splineToLinearHeading(new Pose2d(45, 15, Math.toRadians(0)), Math.toRadians(0))
+                .waitSeconds(2)
                 //move to pick up another sample (3)
-                        .setReversed(true)
-                        .splineToSplineHeading(new Pose2d(62,60, Math.toRadians(230)), Math.toRadians(50))
-                        .waitSeconds(2)
+                .setReversed(true)
+                .splineToSplineHeading(new Pose2d(57,55, Math.toRadians(230)), Math.toRadians(50))
+                .waitSeconds(2)
                 //Deposit sample into basket
-                        .setReversed(false)
-                        .splineToLinearHeading(new Pose2d(24,10,Math.toRadians(0)),Math.toRadians(90))
-                        //park
+                .setReversed(false)
+                .splineToLinearHeading(new Pose2d(24,10,Math.toRadians(0)),Math.toRadians(90))
+                //park
                 .build());
 
 
